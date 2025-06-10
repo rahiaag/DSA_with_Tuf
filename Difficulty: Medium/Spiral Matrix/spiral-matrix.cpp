381012f918ea15438fae1b80ba0ad201b05eead9
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-06-10 23:42:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spiral Matrix/spiral-matrix.cpp
+++ Difficulty: Medium/Spiral Matrix/spiral-matrix.cpp
@@ -7,23 +7,33 @@ class Solution {
         int rowEnd=n-1;
         int colBegin=0;
         int colEnd=m-1;
+        int count=0;
         
         vector<int>ans;
         
         while (rowBegin<=rowEnd && colBegin<=colEnd){
             for (int i=colBegin;i<=colEnd;i++){
-                ans.push_back(a[rowBegin][i]);
+                count++;
+                if (count==k){
+                return a[rowBegin][i];
+            }
             }
             rowBegin++;
             
             for (int i=rowBegin;i<=rowEnd;i++){
-                ans.push_back(a[i][colEnd]);
+                count++;
+                if (count==k){
+                return a[i][colEnd];
+            }
             }
             colEnd--;
             
              if (rowBegin <= rowEnd) {   // for checking single row 
                 for (int i = colEnd; i >= colBegin; i--) {
-                    ans.push_back(a[rowEnd][i]);
+                 count++;
+                if (count==k){
+                return a[rowEnd][i];
+                }
                 }
                 rowEnd--;
             }
@@ -32,15 +42,15 @@ class Solution {
             // Traverse Bottom to Top 
             if (colBegin <= colEnd) {      // for checking single column
                 for (int i = rowEnd; i >= rowBegin; i--) {
-                    ans.push_back(a[i][colBegin]);
+                  count++;
+                if (count==k){
+                return a[i][colBegin];
+                }
                 }
                 colBegin++;
             }
             
         }
-        
-        
-        return ans[k-1];
-        
+        return 0;
     }
 };
