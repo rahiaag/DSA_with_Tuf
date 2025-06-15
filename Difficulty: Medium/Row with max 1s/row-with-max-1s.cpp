ea5b3784452970a0096e4502440090b18b90774c
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-06-15 14:16:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -1,37 +1,18 @@
 // User function template for C++
 class Solution {
   public:
-  int countOne(vector<int>matrix){
-      int n=matrix.size();
-      int count=0;
-      for (int i=0;i<n;i++){
-          if (matrix[i]==1){
-              count++;
-          }
-      }
-      return count;
-  }
-  
     int rowWithMax1s(vector<vector<int>> &arr) {
+        // code here
         int n=arr.size();
         int m=arr[0].size();
-        int maxCount=-1;
-        
-        int row=-1;
-        for (int i=0;i<n;i++){
-            if (countOne(arr[i])>maxCount){
-                maxCount=countOne(arr[i]);
-                row=i;
-            }
-        }
-        return row; 
+     
+     for (int i=0;i<m;i++){
+         for (int j=0;j<n;j++){
+             if (arr[j][i]==1){
+                 return j;
+             }
+         }
+     }
+     return -1;
     }
 };
-
-/*
-
-first appraoch using two loops 
-
-as the array is sorted so we check column wise and in whcihever row i find the first 1 it
-will be my answer
-*/
