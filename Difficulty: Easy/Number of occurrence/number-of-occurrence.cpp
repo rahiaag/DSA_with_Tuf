c21a6e799d600535644dd6846d4f2e9f8bec609c
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-06-17 17:06:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,54 @@
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
+        int index1=-1;
+        
+        // find first occurence
+        while (low<=high){
+            int mid=low+(high-low)/2;
+            
+            if (arr[mid]==target){
+                index1=mid;
+                high=mid-1;
+            }
+            else if (arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        low=0;
+        high=n-1;
+        int index2=-1;
+        
+        // find last index
+         while (low<=high){
+            int mid=low+(high-low)/2;
+            
+            if (arr[mid]==target){
+                index2=mid;
+                low=mid+1;
+            }
+            else if (arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        if (index1==-1)return 0;
+        
+        return index2-index1+1;
+        
+    }
+};
+
+/*
+count method
+unordered_map
+binary search
+*/
