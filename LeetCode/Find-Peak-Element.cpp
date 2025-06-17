65e207a88776cf230ae68476fadd3eef511582d9
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-06-17 17:54:07 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 31.53%] - LeetPush

--- LeetCode/Find-Peak-Element.cpp
+++ LeetCode/Find-Peak-Element.cpp
@@ -1,9 +1,38 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
+        int n = arr.size();
+        int low = 1;   
+        int high = n - 2;
+
+        if (n == 1)
+            return 0;
+        if (arr[0] > arr[1])
+            return 0;
+        if (arr[n - 1] > arr[n - 2])
+            return n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[mid + 1] && arr[mid]>arr[mid-1]) {
+                return mid;
+            } else if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+// time complexiyt o(n)
+
+/*
         int n=arr.size();
         int index=0;
         if (n>=2){
+
         if (arr[0]>arr[1]){
             index=0;
         }
@@ -17,7 +46,5 @@ public:
                 break;
             }
         }
-        
         return index;
-    }
-};
+        */
