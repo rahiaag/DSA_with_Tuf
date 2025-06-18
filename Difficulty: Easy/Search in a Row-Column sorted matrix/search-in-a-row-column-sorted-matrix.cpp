28e1fe23b998adc1ed5fbeef1e0d49d9114872ae
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-06-18 13:12:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Row-Column sorted matrix/search-in-a-row-column-sorted-matrix.cpp
+++ Difficulty: Easy/Search in a Row-Column sorted matrix/search-in-a-row-column-sorted-matrix.cpp
@@ -12,45 +12,22 @@ class Solution {
             if (mat[i][j]==x){
                 return true;
             }
-            else if( mat[i][j] < x ) i++;
-            else  j--;
-        }
-        return false;
-    }
-};
-/*
-Two Approaches
-first is using 2 loops
-second is using binary search
-as the matrix is in increasing order or we can say in sorted order so 
-we will make the 2D array as 1D array and then find the element
-
-3rd approahc is usign inbuilt function
-class Solution {
-  public:
-    bool matSearch(vector<vector<int>> &mat, int x) {
-       for(int i=0;i<mat.size();i++){
-           if(binary_search(mat[i].begin(),mat[i].end(),x)){
-               return true;
-               
-           }
-       }
-       
-       return false;
-    }
-};
-
-*/
-
-/*
-        int n=mat.size();
-        int m=mat[0].size();
-        
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if (mat[i][j]==x){
-                    return true;
-                }
+            else if (mat[i][j]<x){
+                i++;
+            }
+            else{
+                j--;
             }
         }
-        return false;*/
+       return false; 
+    }
+};
+/*
+i) Using two loops
+ii) create a binary search function and for each row apply binary search 
+   i) issi mein chota sa optimization yeh hai ki condition apply karde ki first aur last
+      element ke beech mien agar target nahi aata to binary search apply nahi karna
+      otherwise karna hai
+      
+iii) flattening the 2D matrix into 1D and then searching for the element
+*/
