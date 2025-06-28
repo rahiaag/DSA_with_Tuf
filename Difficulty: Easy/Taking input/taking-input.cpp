27191dce2b858e51ff5527bf918d797610a8f626
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-06-28 22:18:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input/taking-input.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void inputData() {
+        // code here
+        int a;
+        cin>>a;
+        string b;
+        cin>>b;
+        
+        cout<<a<<" "<<b<<endl;
+    }
+};
