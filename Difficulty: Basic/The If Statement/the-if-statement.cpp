49c7d5cd46348f754118d4b14be9135957ae1fc4
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-06-29 14:26:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+
+void utility(int number) {
+    // Write your if statement below
+    if (number<100){
+        cout<<"Number"<<endl;
+    }
+    else{
+    // Write your if statement above
+    cout << "Big" << endl;
+      cout<<"Number"<<endl;
+    }
+}
