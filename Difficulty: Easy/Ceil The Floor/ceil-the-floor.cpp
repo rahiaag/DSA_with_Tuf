5a2b172f7f8f8cea4dcd73bb4d7cbd3328e641bd
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-07-01 17:18:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
@@ -3,27 +3,50 @@
 class Solution {
   public:
     vector<int> getFloorAndCeil(int x, vector<int> &arr) {
-        int n=arr.size();
-        int floor=-1;
-        int ceil=-1;
-        
-        for (int i=0;i<n;i++){
-            if (arr[i]<=x){
-                floor=max(floor,arr[i]);
-            }
-            if (arr[i]>=x){
-                if (ceil==-1){
-                    ceil=arr[i];
-                }
-                else{
-                ceil=min(ceil,arr[i]);
+        // Step 1: Sort the array
+        sort(arr.begin(), arr.end());
+
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int floor = -1, ceil = -1;
+
+        // Binary search for floor
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == x) {
+                floor = arr[mid];
+                break;
+            } else if (arr[mid] < x) {
+                floor = arr[mid];  // possible floor
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+
+        // Reset for ceil
+        low = 0, high = n - 1;
+
+        // Binary search for ceil
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == x) {
+                ceil = arr[mid];
+                break;
+            } else if (arr[mid] > x) {
+                ceil = arr[mid];  // possible ceil
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        return {floor,ceil};
-        
+
+        return {floor, ceil};
     }
 };
+
 
 // if duplicates are present then we are not considering it
 /*
@@ -34,6 +57,6 @@ case 2 maybe duplicates are present so we will also check that condition as well
 
 2ND Using inbuilt function
 
-
+3rd using binary search but only if the array is sorted
 
 */
