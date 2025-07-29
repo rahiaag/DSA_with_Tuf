9d4c9aaccecc2546e335524a2a9c7599fcae2b99
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-07-29 12:35:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Return Size/return-size.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 2;
+    double y = 3.5;
+    char z = 'g';
+
+    // store the size of the variables into a, b, c variables;
+    int a=4;
+    int b=8;
+    int c=1;
+    // printing the size of the variable x.
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
