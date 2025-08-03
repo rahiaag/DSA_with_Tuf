23914e4918e139be98ad9a2f1a1c6c8a1454cb34
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-08-03 23:54:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Evaluate Formulae/evaluate-formulae.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+
+void evalute(int a, int b, int c, int d) {
+    // Complete the code above that stores the output of the expression
+    int res =((a+b)/c)+d;
+        // Complete the code above that stores the output of the expression
+
+        // This prints the output. Don't change it!
+        cout << res << endl;
+}
