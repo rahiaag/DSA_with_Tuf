df362c082fce9ae53a0cb56fb43f1fe4b5f8bbd5
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-08-11 13:46:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete in a Singly Linked List/delete-in-a-singly-linked-list.cpp
+++ Difficulty: Easy/Delete in a Singly Linked List/delete-in-a-singly-linked-list.cpp
@@ -14,21 +14,26 @@ class Solution {
   public:
     /* Function to delete a node from a linked list */
     Node* deleteNode(Node* head, int x) {
-        
-        if (head==NULL){
+        // code here
+        if (head==NULL || head->next==NULL){
             return NULL;
         }
-        
-        if (x==1){
+        Node*temp=head;
+        if (x==1)
+        {
             head=head->next;
+            delete temp;
             return head;
         }
-        Node *temp=head;
         for (int i=1;i<x-1;i++){
             temp=temp->next;
         }
+        if (temp==NULL || temp->next==NULL){
+            return head;
+        }
+        Node*deleteNode=temp->next;
         temp->next=temp->next->next;
-        
+        delete deleteNode;
         return head;
     }
 };
